6a33c2909d83d89c370a8beb85073d36d65bce23
drestew <drestew48@gmail.com>
2022-10-16 04:32:56 -0400

    Fix double render

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,14 @@ import './App.css'
 import './components/Question'
 import {QuestionAnswer, questionInterface} from "./components/Question";
 
-const qArr = [
-    {
-        category: 'heller',
-    },
-    {
-        category: "heller2"
-    },
-    {
-        category: 'heller2'
-    }
-]
-
 function App() {
     const [questions, setQuestions] = useState<questionInterface[]>([])
     useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=10&category=17&difficulty=medium&type=multiple')
             .then(resp => resp.json())
             .then(data => setQuestions(data.results))
-        console.log("render")
-    }, [])
-    // console.log(questions)
 
+    }, [])
 
     const questionList = questions.map(question => {
         return <QuestionAnswer
--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,26 +1,26 @@
 export interface questionInterface {
     category?: string
-    correct_answer?: string
+    correct_answer: string
     difficulty?: string
-    incorrect_answers?: Array<string | number>
-    question?: string
+    incorrect_answers: Array<string | number>
+    question: string
     type?: string
 }
 
 export function QuestionAnswer ({question, correct_answer, incorrect_answers}: questionInterface) {
-    // const randomIndex = Math.ceil(Math.random() * 3)
-    // const answerArrMutable = incorrect_answers // to avoid mutating the incoming prop
-    // // answerArrMutable.splice(randomIndex,0, correct_answer)
-    // const answers = answerArrMutable.map(answer => {
-    //     return <button className="question" key={Math.random() * 100}>{answer}</button>
-    // })
-    // console.log(correct_answer, randomIndex, answerArrMutable)
-    console.log(question)
+    const randomIndex = Math.ceil(Math.random() * 3)
+    const answerArrMutable = incorrect_answers // to avoid mutating the incoming prop
+    answerArrMutable.splice(randomIndex,0, correct_answer)
+
+    const answers = answerArrMutable.map(answer => {
+        return <button className="question" key={Math.random() * 100}>{answer}</button>
+    })
+
     return(
         <div className="question-container">
             <h2>This is the question {question}</h2>
             <div className="answer-container">
-                {/*{answers}*/}
+                {answers}
             </div>
         </div>
     )
